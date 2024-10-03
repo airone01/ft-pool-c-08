4b70cb1a8c169e451fdb922175df4ae2d0ef756d
elagouch <elagouch@42>
2024-10-03 10:19:35 +0200

    feat(ex04): implement

---
+++ ex04/ft_strs_to_tab.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <elagouch@42>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 19:58:23 by elagouch          #+#    #+#             */
+/*   Updated: 2024/10/01 20:06:12 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_stock_str.h"
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	if (*str == '\0')
+		return (0);
+	return (ft_strlen(str + sizeof(char)) + 1);
+}
+
+/*
+ * Duplicates a string.
+ *
+ * Returns:
+ * - string if everything worked
+ * - NULL if malloc failed
+ */
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (dest == 0)
+		return (0);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str	*tab;
+	int			i;
+
+	tab = malloc(sizeof(t_stock_str) * ac);
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].str = av[i];
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].copy = ft_strdup(av[i]);
+		if (tab[i].copy == NULL)
+			return (NULL);
+		i++;
+	}
+  tab[i] = 0;
+	return (tab);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+// }
